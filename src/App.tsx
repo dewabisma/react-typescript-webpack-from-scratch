cb0fb546aafa1917727d1a3d35286a60247bfddb
Dewa Bisma <idewagedebisma@gmail.com>
2021-08-22 21:03:37 +0800

    configure webpack to process img and svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./app.css";
+import IMAGE from "./image.jpg";
+import SVG from "./image.svg";
 
 const App = () => {
   return (
     <div>
       <h1>React TypeScript Webpack From Scratch</h1>
+      <img src={IMAGE} alt="gambar" />
+      <img src={SVG} alt="svg deh" />
     </div>
   );
 };
